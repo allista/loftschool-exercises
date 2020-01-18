5c4f338bd0b8e062e64ebe3b594815640e98e64a
Allis Tauri <allista@gmail.com>
2020-01-18 15:12:41 +0300

    Added header component that displays site title, page nav and logout button
    Would probably be better to extract logout into separate component later

---
+++ loft-taxi/src/components/Header/index.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useCallback } from 'react';
+import classNames from 'classnames';
+import { PageID, PageInfo, Button } from '../../shared';
+
+export type SelectPageCallback = (id: PageID) => void;
+
+export interface HeaderProps {
+  title: string;
+  pages: PageInfo[];
+  currentPage: PageID;
+  selectPage: SelectPageCallback;
+  loggedIn: boolean;
+  logout: () => void;
+}
+
+interface PageButtonProps extends PageInfo {
+  selected: boolean;
+  selectPage: SelectPageCallback;
+}
+
+const PageButton: FC<PageButtonProps> = ({ id, title, selected, selectPage }) => {
+  const onClick = useCallback(() => selectPage(id), [id]);
+  const className = classNames({ 'loft-taxi-page-selected': selected }, 'loft-taxi-header-page');
+  return (
+    <Button className={className} onClick={onClick}>
+      {title}
+    </Button>
+  );
+};
+
+export const Header: FC<HeaderProps> = ({
+  title,
+  pages,
+  currentPage,
+  selectPage,
+  loggedIn,
+  logout,
+}) => {
+  const pageButtons = pages.map(p =>
+    loggedIn && (p.id === PageID.LOGIN || p.id === PageID.REGISTRATION) ? null : (
+      <PageButton {...p} selected={p.id === currentPage} selectPage={selectPage} />
+    ),
+  );
+  return (
+    <div className="loft-taxi-header">
+      <div className="loft-taxi-header-title">{title}</div>
+      {pageButtons}
+      {loggedIn ? (
+        <Button className="loft-taxi-logout-button" onClick={logout}>
+          Выйти
+        </Button>
+      ) : null}
+    </div>
+  );
+};
+
+export default Header;
--- loft-taxi/src/shared/index.ts
+++ loft-taxi/src/shared/index.ts
@@ -1,1 +1,13 @@
 export * from './Button';
+
+export enum PageID {
+  LOGIN = 'login',
+  PROFILE = 'profile',
+  MAP = 'map',
+  REGISTRATION = 'registration',
+}
+
+export type PageInfo = {
+  id: PageID;
+  title: string;
+};
