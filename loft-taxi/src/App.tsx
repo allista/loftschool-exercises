e1cb579b0a70d2a7b7c04ebe1e3d1bea40fb3494
Allis Tauri <allista@gmail.com>
2020-01-18 15:16:06 +0300

    Added shared const titles of the site and the pages. Made the toplevel App.

--- loft-taxi/src/App.tsx
+++ loft-taxi/src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import Header from './components/Header';
+import PageSelector from './components/PageSelector';
+import { pageTitles, PageID, title } from './shared';
+
+export const pages = [PageID.LOGIN, PageID.MAP, PageID.PROFILE, PageID.REGISTRATION].map(p => ({
+  id: p,
+  title: pageTitles[p],
+}));
 
 const App: React.FC = () => {
+  const [pageID, setPage] = useState(pages[0].id);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loft-taxi-main-page">
+      <Header
+        title={title}
+        pages={pages}
+        currentPage={pageID}
+        selectPage={setPage}
+        loggedIn={false}
+        logout={() => null}
+      />
+      <h2>{pageTitles[pageID]}</h2>
+      <PageSelector pageID={pageID} />
     </div>
   );
 };
--- loft-taxi/src/shared/index.ts
+++ loft-taxi/src/shared/index.ts
@@ -1,5 +1,7 @@
 export * from './Button';
 
+export const title = 'Loft Taxi';
+
 export enum PageID {
   LOGIN = 'login',
   PROFILE = 'profile',
@@ -10,4 +12,11 @@ export enum PageID {
 export type PageInfo = {
   id: PageID;
   title: string;
+};
+
+export const pageTitles = {
+  [PageID.LOGIN]: 'Войти',
+  [PageID.PROFILE]: 'Профиль',
+  [PageID.MAP]: 'Карта',
+  [PageID.REGISTRATION]: 'Зарегистрироваться',
 };
