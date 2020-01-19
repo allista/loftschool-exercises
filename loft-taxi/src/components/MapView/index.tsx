0ac16e09b7254ae35e8809dc08fe44ca67766e3a
Allis Tauri <allista@gmail.com>
2020-01-20 01:39:27 +0300

    Updated MapView and Profile stubs to be better visible above the background

--- loft-taxi/src/components/MapView/index.tsx
+++ loft-taxi/src/components/MapView/index.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react';
+import { PageID, pageMap } from '../../shared';
+import './style.scss';
 
 export interface MapViewProps {}
 
-export const MapView: FC<MapViewProps> = props => {
-  return null;
+export const MapView: FC<MapViewProps> = ({}) => {
+  return (
+    <div className="loft-taxi-map">
+      <div className="loft-taxi-page-title">{pageMap[PageID.MAP].title}</div>
+    </div>
+  );
 };
 
 export default MapView;
