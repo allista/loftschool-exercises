dde9a628cf1097d2b555ce5f0ec3fab6ec08914d
Allis Tauri <allista@gmail.com>
2020-01-27 01:12:09 +0300

    Catch runtime errors in case of missing logo

--- loft-taxi/src/components/Logo/index.tsx
+++ loft-taxi/src/components/Logo/index.tsx
@@ -12,7 +12,13 @@ export interface LogoProps {
 }
 
 export const Logo: FC<LogoProps> = ({ name = LogoName.DEFAULT }) => {
-  const logoUrl = require(`./${name}.svg`);
+  let logoUrl: string;
+  try {
+    logoUrl = require(`./${name}.svg`);
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
   return (
     <div className={classNames('loft-taxi-logo', name)}>
       <img className="loft-taxi-logo-icon" src={logoUrl} alt={`logo-${name}`} />
