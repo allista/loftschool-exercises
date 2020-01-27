25527b7b5d43320744211e74479e3d5e1d49d7dc
Allis Tauri <allista@gmail.com>
2020-01-27 03:38:28 +0300

    Corrected NavContext props interface name

--- loft-taxi/src/shared/contexts/NavContext.ts
+++ loft-taxi/src/shared/contexts/NavContext.ts
@@ -1,13 +1,13 @@
 import React from 'react';
 import { PageID, PageInfo } from '../types';
 
-export interface NavContext {
+export interface NavContextProps {
   currentPageID: PageID;
   pages: PageInfo[];
   selectPage: (pageID: PageID) => void;
 }
 
-export const NavContext = React.createContext<NavContext>({
+export const NavContext = React.createContext<NavContextProps>({
   currentPageID: PageID.LOGIN,
   pages: [],
   selectPage: () => {},
