274cecd9073bd0f97818222b84cf189262b87545
Evgeny Taranov <taranov@jet.su>
2020-02-03 10:59:35 +0300

    Added ErrorDisplay to the App

--- loft-taxi/src/App.tsx
+++ loft-taxi/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.scss';
+import ErrorDisplay from 'components/ErrorDisplay';
 import PageSelector from 'components/PageSelector';
 import React, { FC } from 'react';
 
 const App: FC = () => {
   return (
+    <>
+      <ErrorDisplay />
       <div className="loft-taxi-main-page">
         <PageSelector />
       </div>
+    </>
   );
 };
 
